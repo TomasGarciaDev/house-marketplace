b87269f252ee1b5abfeb5167d6f4418a7ea86ffd
TomasGarciaDev <tomasgarciadev@gmail.com>
2024-11-12 17:14:56 +0100

    User Sign In

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,14 @@
+import { useEffect, useState } from "react";
+import { getAuth } from "firebase/auth";
+
 function Profile() {
-  return <div>Profile</div>;
+  const [user, setUser] = useState(null);
+
+  const auth = getAuth();
+  useEffect(() => {
+    setUser(auth.currentUser);
+  }, []);
+  return <h1>{user ? user.displayName : "Not Logged In"}</h1>;
 }
 
 export default Profile;
