b40b6574750cd7d10da48527d7a65e273a9f02d0
TomasGarciaDev <tomasgarciadev@gmail.com>
2024-11-15 18:37:37 +0100

    Google OAuth

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
-import Navbar from "./pages/components/Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
