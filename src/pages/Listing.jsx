5d7661e03e65ded0cb905d3083a7142dfa515729
TomasGarciaDev <tomasgarciadev@gmail.com>
2024-12-16 13:27:59 +0100

    Leaflet Map

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { getDoc, doc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { db } from "../firebase.config";
@@ -69,10 +70,9 @@ function Listing() {
         </p>
         {listing.offer && (
           <p className='discountPrice'>
-            ${listing.regularPrice - listing.discountedPrice}
+            ${listing.regularPrice - listing.discountedPrice} discount
           </p>
         )}
-
         <ul className='listingDetailsList'>
           <li>
             {listing.bedrooms > 1
@@ -90,7 +90,25 @@ function Listing() {
 
         <p className='listingLocationTitle'>Location</p>
 
-        {/* MAP */}
+        <div className='leafletContainer'>
+          <MapContainer
+            style={{ height: "100%", width: "100%" }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+            />
+
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}
+            >
+              <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
 
         {auth.currentUser?.uid !== listing.userRef && (
           <Link
